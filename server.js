df33911feface3796b9a46a8866005f497c9d129
Daniel Ojerahi <ojerahidaniel89@gmail.com>
2026-02-15 14:33:58 +0100

    added json

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 
 /* -------------------------------
-   Stage 1: Deploy working server
+   Middleware
+-------------------------------- */
+
+// This enables JSON requests & responses
+app.use(express.json());
+
+/* -------------------------------
+   Routes
 -------------------------------- */
 
 app.get("/", (req, res) => {
-  res.send("Server is working 🚀");
+  res.json({
+    message: "Server is working 🚀"
+  });
 });
+
+/* -------------------------------
+   Start Server
+-------------------------------- */
 
 const PORT = process.env.PORT || 3000;
 const HOST = "http://localhost";
